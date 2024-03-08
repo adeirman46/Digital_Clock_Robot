323510fd24be8c26d0322c133f71873aa6b071d8
Adeirman46 <adeirman387@gmail.com>
2024-03-08 10:26:25 +0700

    vib_sensor

---
+++ 8mar_vibration_sensor/8mar_vibration_sensor.ino
@@ -0,0 +1,18 @@
+const int sensor_pin = 10;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensor_pin, INPUT);
+
+}
+
+void loop() {
+  if (digitalRead(sensor_pin)) {
+    Serial.println("vibration detected");
+  }
+  else {
+    Serial.println("vibration not detected");
+  }
+  delay(100);
+
+}
